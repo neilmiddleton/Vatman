96e02d1ed1dcc92bb1234e5116645d073280984f
Neil Middleton <neil@kyanmedia.com>
2012-01-04 17:49:55 +0000

    Improve tests

--- spec/vatman_spec.rb
+++ spec/vatman_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 
 describe Vatman do
   it 'should return details of a valid VAT number' do
+
+    cl = {}
+    resp = {}
+    resp[:check_vat_response] = {}
+    resp[:check_vat_response][:valid] = true
+    resp[:check_vat_response][:name] = "KYANMEDIA LTD"
+    resp[:check_vat_response][:address] = "171 HIGH STREET\nGUILDFORD\nSURREY\n\n\nGU1 3AJ"
+    cl.stub(:request){resp}
+    Savon::Client.stub(:new){cl}
+
     v = Vatman::Check.new("GB", 800634860)
     v.should be_valid
     v.name.should == 'KYANMEDIA LTD'
@@ -9,16 +19,37 @@ describe Vatman do
   end
 
   it 'should accept string formatted numbers' do
+    cl = {}
+    resp = {}
+    resp[:check_vat_response] = {}
+    resp[:check_vat_response][:valid] = true
+    cl.stub(:request).with(:tnsl, :check_vat, :body => {:country_code => "GB", :vat_number => "800634860"}){resp}
+    Savon::Client.stub(:new){cl}
+
     v = Vatman::Check.new("GB", "800  6348    60") 
     v.should be_valid
   end
 
   it 'should reject invalid VAT numbers' do
+    cl = {}
+    resp = {}
+    resp[:check_vat_response] = {}
+    resp[:check_vat_response][:valid] = false
+    cl.stub(:request).with(:tnsl, :check_vat, :body => {:country_code => "GB", :vat_number => "12345679"}){resp}
+    Savon::Client.stub(:new){cl}
+
     v = Vatman::Check.new("GB", 12345679)
     v.should_not be_valid
   end
 
   it 'should reject invalid string vat numbers' do
+    cl = {}
+    resp = {}
+    resp[:check_vat_response] = {}
+    resp[:check_vat_response][:valid] = false
+    cl.stub(:request).with(:tnsl, :check_vat, :body => {:country_code => "GB", :vat_number => "abcdefghijkl"}){resp}
+    Savon::Client.stub(:new){cl}
+    
     v = Vatman::Check.new("GB", "abcdefghijkl")
     v.should_not be_valid
   end
